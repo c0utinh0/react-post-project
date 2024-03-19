783b45db1eabf83b1b088d33cf33eb7c5b08aa89
Douglas Coutinho <douglas-couto@hotmail.com>
2024-03-19 11:08:14 -0300

    Class Component Stateful

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    counter: 0
+  }
+
+  handlerImgClick = () => {
+    const { counter } = this.state;
+    this.setState({ counter: counter + 1 })
+  }
+  
+  render(){
+    const { counter } = this.state;
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img  src={logo} className="App-logo" alt="logo" />
+          <p onClick={this.handlerImgClick}>### Clique Aqui ###</p>
+          <p> 
+            {counter}
+          </p>
+          <p >
+            Edit <code>src/App.js</code> and save to reload. 
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
