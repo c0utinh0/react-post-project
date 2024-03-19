25dc5758f37231eec1b657e56c8c99cbdf5ca786
Douglas Coutinho <douglas-couto@hotmail.com>
2024-03-19 12:36:36 -0300

    Lifecicle Method componentDidMount and Fetch

--- src/App.js
+++ src/App.js
@@ -3,42 +3,51 @@ import { Component } from 'react';
 
 class App extends Component {
   state = {
-    posts: [
-      {
-        id: 1,
-        title: 'JavaScript',
-        body: 'JavaScript is very good'
-      },
-      {
-        id: 2,
-        title: 'React',
-        body: 'Nice'
-      },
-      {
-        id: 3,
-        title: 'React Native',
-        body: 'To mobile'
-      }
-    ]
+    posts: []
+  }
+
+  componentDidMount(){
+    this.loadPosts();
+  }
+
+  loadPosts = async () => {
+    const postsResponse = fetch('https://jsonplaceholder.typicode.com/posts');
+    const photosResponse = fetch('https://jsonplaceholder.typicode.com/photos');
+  
+    const [posts, photos] = await Promise.all([postsResponse, photosResponse]);
+
+    const postsJson = await posts.json();
+    const photosJson = await photos.json();
+
+    // Feito pq a qnt de photo é maior q a de posts 
+    const postsAndPhotos = postsJson.map((post, index) => {
+      return {...post, cover: photosJson[index].url}
+    })
+    this.setState({posts: postsAndPhotos})
+  
   }
   
   render(){
     const { posts } = this.state;
 
     return (
-      <div className="App">
+      <section className='container'>
+        <div className="posts">
         {
           posts.map(post => (
-            <div key={post.id}>
-              <h1>{post.title}</h1>
-              <p>{post.body}</p>
+            <div className='post'>
+              <img src={post.cover} alt={post.title} />
+              <div key={post.id} className='post-content'>
+                <h1>{post.title}</h1>
+                <p>{post.body}</p>
+              </div>
             </div>
           ))
         }
-      </div>
+        </div>
+      </section>      
     );
-  }
-  
+  }  
 }
 
 export default App;
